89ecd6fbd4b9fb327e719ec8b9910ec0c303faab
Thomas Toro <thomy0736@gmail.com>
2025-11-08 00:02:32 -0600

    Se termina el proyecto de CV en su totalidad

--- script.js
+++ script.js
@@ -6,3 +6,20 @@ let header = document.querySelector('.header');
 
 header.classList.toggle("sticky", window.scrollY > 100);
 };
+
+/*SWIPER TESTIMONIAL*/
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 2,
+    spaceBetween: 50,
+    loop: true,
+    grabCursor: true,
+    pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    },
+    navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+    },
+});
