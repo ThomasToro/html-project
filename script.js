75029b3833f16f414e9d94b9e6432a07d8a53ab0
Thomas Toro <thomy0736@gmail.com>
2025-11-14 21:29:19 -0600

    Se agregan media queries y scss al proyecto

--- script.js
+++ script.js
@@ -4,14 +4,23 @@ window.onscroll = ()=> {
 
 let header = document.querySelector('.header');
 
-header.classList.toggle("sticky", window.scrollY > 100);
+header.classList.toggle("sticky", window.scrollY > 1);
+};
+
+// Toggle menu icon
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
 };
 
 /*SWIPER TESTIMONIAL*/
 
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 2,
-    spaceBetween: 50,
+    slidesPerView: 1,
+    spaceBetween: 80,
     loop: true,
     grabCursor: true,
     pagination: {
